7c67233cdb1b090038dd73209cdf18f302d05517
sb <oluwasubomi.bashoru@bison.howard.edu>
2024-02-13 23:42:30 -0600

    commit

---
+++ Flixster Pt. 1/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flixster Pt. 1
+//
+//   created by Oluwasubomi Bashorun on 02/13/2024.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
